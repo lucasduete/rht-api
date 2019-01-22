2a5ca88e842fe3284a5b70fcd59f3ff1b025e46d
Lucas Duete <lm.duete@gmail.com>
2019-01-22 17:48:25 -0300

    Implemented Training rest Controller

---
+++ src/main/java/io/github/lucasduete/rhtapi/controllers/TrainingController.java
@@ -0,0 +1,31 @@
+package io.github.lucasduete.rhtapi.controllers;
+
+import io.github.lucasduete.rhtapi.models.Training;
+import io.github.lucasduete.rhtapi.services.TrainingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("training")
+public class TrainingController {
+
+    private final TrainingService service;
+
+    public TrainingController(TrainingService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity saveTraining(@RequestBody Training training) {
+        if (training == null) return ResponseEntity.badRequest().body("Training can't be null");
+
+        Training savedTraining = this.service.save(training);
+
+        if (savedTraining != null) return ResponseEntity.status(HttpStatus.CREATED).body(savedTraining);
+        else return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+}
