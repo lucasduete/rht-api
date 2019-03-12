ae0b526877da2947523311e5bd14673bffab7d7a
Lucas Duete <lm.duete@gmail.com>
2019-03-12 17:46:13 -0300

    Implemented Indicator Service for Vacancy and Rating indicatos

---
+++ src/main/java/io/github/lucasduete/rhtapi/services/IndicatorService.java
@@ -0,0 +1,41 @@
+package io.github.lucasduete.rhtapi.services;
+
+import io.github.lucasduete.rhtapi.indicators.implementations.RatingIndicator;
+import io.github.lucasduete.rhtapi.models.Training;
+import io.github.lucasduete.rhtapi.repositories.TrainingRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class IndicatorService {
+
+    private final TrainingRepository trainingRepository;
+
+    public IndicatorService(TrainingRepository trainingRepository) {
+        this.trainingRepository = trainingRepository;
+    }
+
+    public String calculateRatingIndicator(Long idTraining) {
+
+        Optional<Training> trainingOptional = this.trainingRepository.findById(idTraining);
+
+        if (!trainingOptional.isPresent()) return null;
+
+        Training training = trainingOptional.get();
+
+        return new RatingIndicator(training).calculate();
+    }
+
+    public String calculateVacancyIdicator(Long idTraining) {
+
+        Optional<Training> trainingOptional = this.trainingRepository.findById(idTraining);
+
+        if (!trainingOptional.isPresent()) return null;
+
+        Training training = trainingOptional.get();
+
+        return new RatingIndicator(training).calculate();
+    }
+
+}
