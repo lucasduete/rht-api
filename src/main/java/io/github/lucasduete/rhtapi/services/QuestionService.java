7fa14a3a01f857db3fc75fe7abf02ba8aa8724d1
Lucas Duete <lm.duete@gmail.com>
2019-03-17 13:06:27 -0300

    Refatored method to save a question in QuestionService to add this question in a Training.

--- src/main/java/io/github/lucasduete/rhtapi/services/QuestionService.java
+++ src/main/java/io/github/lucasduete/rhtapi/services/QuestionService.java
@@ -1,28 +1,44 @@
 package io.github.lucasduete.rhtapi.services;
 
 import io.github.lucasduete.rhtapi.models.Question;
+import io.github.lucasduete.rhtapi.models.Training;
 import io.github.lucasduete.rhtapi.repositories.QuestionRepository;
+import io.github.lucasduete.rhtapi.repositories.TrainingRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class QuestionService {
 
-    private final QuestionRepository repository;
+    private final QuestionRepository questioRepository;
+    private final TrainingRepository trainingRepository;
 
-    public QuestionService(QuestionRepository repository) {
-        this.repository = repository;
+    public QuestionService(QuestionRepository questioRepository, TrainingRepository trainingRepository) {
+        this.questioRepository = questioRepository;
+        this.trainingRepository = trainingRepository;
     }
 
-    public Question save(Question question) {
-        return this.repository.save(question);
+    public Question save(Question question, Long trainingId) {
+
+        Optional<Training> trainingOptional = this.trainingRepository.findById(trainingId);
+
+        if (!trainingOptional.isPresent()) return null;
+
+        Training training = trainingOptional.get();
+
+        training.addQuestion(question);
+        this.trainingRepository.save(training);
+
+        return question;
     }
 
     public Question getQuestion(Long id) {
-        return this.repository.getOne(id);
+        return this.questioRepository.getOne(id);
     }
 
     public void remove(Long id) {
-        this.repository.deleteById(id);
+        this.questioRepository.deleteById(id);
     }
 
 }
