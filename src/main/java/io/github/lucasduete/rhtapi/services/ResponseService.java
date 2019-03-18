1427eb504d843604ef0951cd8b464a4f69248c35
Lucas Duete <lm.duete@gmail.com>
2019-03-17 22:27:34 -0300

    Implemented method to persiste a ResponseModel in ResponseService

--- src/main/java/io/github/lucasduete/rhtapi/services/ResponseService.java
+++ src/main/java/io/github/lucasduete/rhtapi/services/ResponseService.java
@@ -1,20 +1,42 @@
 package io.github.lucasduete.rhtapi.services;
 
+import io.github.lucasduete.rhtapi.controllers.valueobjects.ResponseModel;
+import io.github.lucasduete.rhtapi.models.Employee;
+import io.github.lucasduete.rhtapi.models.Question;
 import io.github.lucasduete.rhtapi.models.Response;
 import io.github.lucasduete.rhtapi.repositories.ResponseRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class ResponseService {
 
-    private final ResponseRepository repository;
+    private final ResponseRepository responseRepository;
+    private final QuestionService questionService;
+    private final EmployeeService employeeService;
 
-    public ResponseService(ResponseRepository respository) {
-        this.repository = respository;
+    public ResponseService(ResponseRepository responseRepository, QuestionService questionService, EmployeeService employeeService) {
+        this.responseRepository = responseRepository;
+        this.questionService = questionService;
+        this.employeeService = employeeService;
     }
 
+    @Transactional(Transactional.TxType.REQUIRED)
     public Response save(Response response) {
-        return this.repository.save(response);
+        return this.responseRepository.save(response);
+    }
+
+    @Transactional(Transactional.TxType.REQUIRES_NEW)
+    public Response save(ResponseModel responseModel) {
+
+        Employee employee = this.employeeService.findByEmail(responseModel.getEmailEmployee());
+
+        Question question = this.questionService.getQuestion(responseModel.getIdQuestion());
+
+        return this.save(
+                new Response(null, responseModel.getContent(), employee, question)
+        );
     }
 
 }
