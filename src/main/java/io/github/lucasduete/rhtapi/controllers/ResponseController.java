2b08a8866cc7cfc66bcf3cf0939e2c89ea3af63e
Lucas Duete <lm.duete@gmail.com>
2019-03-17 22:27:58 -0300

    Implemented method to persist a list of response in ResponseController

--- src/main/java/io/github/lucasduete/rhtapi/controllers/ResponseController.java
+++ src/main/java/io/github/lucasduete/rhtapi/controllers/ResponseController.java
@@ -1,5 +1,6 @@
 package io.github.lucasduete.rhtapi.controllers;
 
+import io.github.lucasduete.rhtapi.controllers.valueobjects.ResponseModel;
 import io.github.lucasduete.rhtapi.models.Response;
 import io.github.lucasduete.rhtapi.services.ResponseService;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("response")
@@ -20,7 +24,7 @@ public class ResponseController {
     }
 
     @PostMapping
-    public ResponseEntity saveResponse(@RequestBody Response response) {
+    public ResponseEntity saveResponse(@RequestBody ResponseModel response) {
         if (response == null) return ResponseEntity.badRequest().body("Reponse can't be null");
 
         Response savedResponse = this.service.save(response);
@@ -28,4 +32,26 @@ public class ResponseController {
         if (savedResponse != null) return ResponseEntity.status(HttpStatus.CREATED).body(response);
         else return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
+
+    @PostMapping("list")
+    public ResponseEntity saveResponses(@RequestBody List<ResponseModel> responses) {
+        if (responses == null || responses.isEmpty())
+            return ResponseEntity.badRequest().body("Reponse can't be null and can't be empty");
+
+
+        List<Response> responsesSaved = new ArrayList<>();
+
+        responses.forEach( responseModel -> {
+            if (responseModel != null)
+                responsesSaved.add(this.service.save(responseModel));
+        });
+
+
+        if (!responsesSaved.isEmpty()){
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 }
